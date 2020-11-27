c2b03bb4a7f7e27739500722456c30c7f224b213
vaad19uj <vaad19uj@student.ju.se>
2020-11-27 18:41:57 +0100

    level bubble

--- Core/Src/stm32f4xx_hal_msp.c
+++ Core/Src/stm32f4xx_hal_msp.c
@@ -209,7 +209,7 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
     PA6     ------> TIM3_CH1
     PA7     ------> TIM3_CH2
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7;
+    GPIO_InitStruct.Pin = xLED_Pin_Pin|yLED_Pin_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
